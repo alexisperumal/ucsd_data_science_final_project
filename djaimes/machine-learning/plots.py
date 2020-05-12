ed1dd1209d9689b87d4fdd4113fac90d0acf829c
David Jaimes <david@djaimes.com>
2020-05-11 22:55:09 -0700

    add comparison and confucion matrices for all models in presentation and website page.

--- djaimes/machine-learning/plots.py
+++ djaimes/machine-learning/plots.py
@@ -17,9 +17,17 @@ RFC_cm = {
 	'TP': 8
 }
 
+SVC_cm = {
+	'TN': 1270,
+	'FP': 3,
+	'FN': 131,
+	'TP': 7
+}
+
 df = pd.DataFrame({
 	'LR': fn.confusion_matrix_calc(LR_cm),
 	'RFC': fn.confusion_matrix_calc(RFC_cm),
+	'SVC': fn.confusion_matrix_calc(SVC_cm),
 	})
 
 
@@ -32,9 +40,12 @@ for i, (index, row) in enumerate(df.iterrows()):
     plt.text(row['LR'] + 0.22, i - 0.08,
         f"{round(100 * row['LR'], 1):<04}%",
         va='top', ha='right', fontsize=18, color='C0')
-    plt.text(row['RFC'] + 0.22, i + 0.2,
+    plt.text(row['RFC'] + 0.22, i + 0.07,
         f"{round(100 * row['RFC'], 1):<04}%",
         va='top', ha='right', fontsize=18, color='C1')
+    plt.text(row['SVC'] + 0.22, i + 0.24,
+        f"{round(100 * row['SVC'], 1):<04}%",
+        va='top', ha='right', fontsize=18, color='C2')
 
 plt.xlim(0, 1.2)
 plt.gca().set_yticklabels(df.index, fontsize=18)
